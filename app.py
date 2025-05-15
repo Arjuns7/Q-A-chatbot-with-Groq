5f9ff20267c1e393d8f637ea246eff6909469b41
Arjuns7 <88701543+Arjuns7@users.noreply.github.com>
2025-05-15 22:46:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+#env setup
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2']= "true"
 os.environ["LANGCHAIN_PROJECT"] = "Q&A Chatbot with Groq"
